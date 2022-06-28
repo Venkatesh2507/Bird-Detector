dfe8186b3b86ec5e4580dfe3423e71050de0ac08
Venkatesh2507 <96984587+Venkatesh2507@users.noreply.github.com>
2022-06-28 12:59:59 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/birddetector/MainActivity.java
+++ app/src/main/java/com/example/birddetector/MainActivity.java
@@ -28,7 +28,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    Button loadImage;
+    Button loadImage;      /*intializing the widgets*/
     ImageView birdImage;
     TextView result;
     TextView link;
@@ -41,14 +41,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        loadImage = findViewById(R.id.loadImage);
-        birdImage = findViewById(R.id.imageView);
+        setContentView(R.layout.activity_main);  /*assigning the id's to the widgets*/
+        loadImage = findViewById(R.id.loadImage);    
+        birdImage = findViewById(R.id.imageView);        
         result = findViewById(R.id.resultTextView);
         link = findViewById(R.id.linkTextView);
         camera = findViewById(R.id.cameraButton);
         volume = findViewById(R.id.sound);
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() { /*text to speech class used so as to spell the scientific
+                                                                                                        name of the bird*/
             @Override
             public void onInit(int i) {
                 if(i!=TextToSpeech.ERROR)
@@ -77,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
      camera.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
-              Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+              Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); /* intent started so as to open the camera and click a pic  of bird*/
               startActivityForResult(camera_intent,pic_id);
          }
      });
@@ -87,8 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI); /*intent started so as to open the gallery and pick the image of 
+        startActivityForResult(gallery, PICK_IMAGE);                                                   of bird */
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
@@ -97,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 imageUri = data.getData();
                 Bitmap imageBitmap=null;
                 try {
-                    imageBitmap = URItoBitmap(imageUri);
+                    imageBitmap = URItoBitmap(imageUri);  /* this case defines when the image is clicked through camera and image uri is converted to bitmap*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -108,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
             }
                  break;
             case 2: if (resultCode==RESULT_OK){
-                Bundle bundle = data.getExtras();
+                Bundle bundle = data.getExtras();           /* this case defines when the image is taken through gallery */
                 Bitmap bitmapImage = (Bitmap) bundle.get("data");
                 birdImage.setImageBitmap(bitmapImage);
                 outputGenerator(bitmapImage);
@@ -117,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void outputGenerator(Bitmap imageBitmap) {
         try {
-            LiteModelAiyVisionClassifierBirdsV13 model = LiteModelAiyVisionClassifierBirdsV13.newInstance(MainActivity.this);
+            LiteModelAiyVisionClassifierBirdsV13 model = LiteModelAiyVisionClassifierBirdsV13.newInstance(MainActivity.this);    //Machine learning model used
 
             // Creates inputs for reference.
             TensorImage image = TensorImage.fromBitmap(imageBitmap);
@@ -125,7 +126,8 @@ public class MainActivity extends AppCompatActivity {
             // Runs model inference and gets result.
             LiteModelAiyVisionClassifierBirdsV13.Outputs outputs = model.process(image);
             List<Category> probability = outputs.getProbabilityAsCategoryList();
-
+            
+            // calculates the maximum probable case from the listed probablities
             int index=0;
             float max=probability.get(0).getScore();
             for (int i=0;i< probability.size();i++) {
@@ -145,8 +147,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private Bitmap URItoBitmap(Uri imageUri) throws IOException {
-        return MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+    private Bitmap URItoBitmap(Uri imageUri) throws IOException {          
+        return MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);  //image uri converted to bitmap
     }
 
 }
